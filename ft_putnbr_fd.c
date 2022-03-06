74400879620edeb5d329754a956c191593bf83c4
nikanyx <nikanyx@protonmail.com>
2022-03-06 21:02:41 +0000

    fixed putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cmachado <cmachado@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cmachado <cmachado@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 16:21:29 by cmachado          #+#    #+#             */
-/*   Updated: 2022/03/05 16:38:15 by cmachado         ###   ########.fr       */
+/*   Updated: 2022/03/06 20:57:40 by cmachado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,26 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*new;
+	char	c;
 
-	new = ft_itoa(n);
-	write(fd, &*new, ft_strlen(new));
+	if (n == -2147483648)
+	{
+		write(fd, "-2", 2);
+		n = 147483648;
+	}
+	else if (n < 0)
+	{
+		write(fd, "-", 1);
+		n *= -1;
+	}
+	if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+	{
+		c = n + 48;
+		write(fd, &c, 1);
+	}
 }
