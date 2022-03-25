235dfa15c73351457fc752d84bed5791f621409e
nikanyx <nikanyx@protonmail.com>
2022-03-25 21:27:15 +0000

    finished lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: cmachado <cmachado@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 19:44:37 by cmachado          #+#    #+#             */
-/*   Updated: 2022/03/25 20:23:35 by cmachado         ###   ########.fr       */
+/*   Updated: 2022/03/25 21:25:11 by cmachado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,17 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new;
 	t_list	*node;
 
+	new = NULL;
 	while (lst)
 	{
 		node = ft_lstnew(f(lst->content));
+		if (!node)
+		{
+			ft_lstclear(&new, del);
+			return (NULL);
+		}
 		ft_lstadd_back(&new, node);
-		del(node);
+		lst = lst->next;
 	}
 	return (new);
 }
